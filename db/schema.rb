86e6f6b6d3299712d58f930959d4e2ecfcea1e2c
chantellebecker <chantellewelp@gmail.com>
2016-10-28 13:58:35 -0400

    hero image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027151655) do
+ActiveRecord::Schema.define(version: 20161028171039) do
 
   create_table "events", force: :cascade do |t|
     t.string   "location"
@@ -37,8 +37,9 @@ ActiveRecord::Schema.define(version: 20161027151655) do
     t.string   "age"
     t.string   "players"
     t.string   "table_size"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "users", force: :cascade do |t|
