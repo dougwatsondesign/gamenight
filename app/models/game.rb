dfcb7a51855bd97c944eaf5f186ff8c6ce63786e
chantellebecker <chantellewelp@gmail.com>
2016-10-31 11:20:02 -0400

    games index edit page

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,9 @@
 class Game < ApplicationRecord
 	# has_many :events
 
+	# has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:styles/missing.png"
+ #  	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 	validates :title, presence: true
 	validates :games_type, presence: true
 	validates :category, presence: true
